962e72c1d7e58351f6c9e1efbc142c5724c95471
Bernhard Reuter <43750470+msmdev@users.noreply.github.com>
2020-04-20 21:14:14 +0200

    [util/sorted_schur] bugfix top_eigenvalues()

--- msmtools/util/sorted_schur.py
+++ msmtools/util/sorted_schur.py
@@ -32,6 +32,20 @@ def top_eigenvalues(P, m, z='LM'):
         top_eigenvals, _ = eigs(P, k=m+1, which=z)
         if np.any(np.isnan(top_eigenvals)):
             raise ValueError("Some of the top m eigenvalues of P are NaN!")
+        if (z == 'LM'):
+            eigenval_in = top_eigenvals[m-1]
+            eigenval_out = top_eigenvals[m]
+            # Don't separate conjugate eigenvalues (corresponding to 2x2-block in R).
+            if np.isclose(eigenval_in, np.conj(eigenval_out)):
+                raise ValueError("Clustering into " + str(m) + " clusters will split conjugate eigenvalues! "
+                                 + " Request one cluster more or less.")
+        elif (z == 'LR'):
+            eigenval_in = np.real(top_eigenvals[m-1])
+            eigenval_out = np.real(top_eigenvals[m])
+            # Don't separate conjugate eigenvalues (corresponding to 2x2-block in R).
+            if np.isclose(eigenval_in, eigenval_out):
+                raise ValueError("Clustering into " + str(m) + " clusters will split conjugate eigenvalues! "
+                                 + " Request one cluster more or less.")
     else: 
         eigenvals = np.linalg.eigvals(P)
         if np.any(np.isnan(eigenvals)):
