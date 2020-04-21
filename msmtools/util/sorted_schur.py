e3c67c9b5326481df46ea744aa57bd693455a730
Bernhard Reuter <43750470+msmdev@users.noreply.github.com>
2020-04-21 09:22:23 +0200

    [util/sorted_schur/sorted_scipy_schur] cutoff changed again

--- msmtools/util/sorted_schur.py
+++ msmtools/util/sorted_schur.py
@@ -131,13 +131,13 @@ def sorted_scipy_schur(P, m, z='LM'):
     if z == 'LM':
         # Determine the cutoff for sorting in schur().
         #cutoff = (np.abs(eigenval_in) + np.abs(eigenval_out)) / 2.0 
-        cutoff = np.abs(top_eigenvals[m+1])
+        cutoff = np.abs(top_eigenvals[m-1]) - 0.1
 
         R, Q, sdim = schur(P, sort=lambda x: np.abs(x) > cutoff)
     elif z == 'LR':
         # Determine the cutoff for sorting in schur().
         #cutoff = (np.real(eigenval_in) + np.real(eigenval_out)) / 2.0 
-        cutoff = np.real(top_eigenvals[m+1])
+        cutoff = np.real(top_eigenvals[m-1]) - 0.1
 
         R, Q, sdim = schur(P, sort=lambda x: np.real(x) > cutoff)
     
